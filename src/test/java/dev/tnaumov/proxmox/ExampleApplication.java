718ef26fd37b9e6b324523a42ba2c5b5ff9151e4
Tim Naumov <tnaumov.dev@gmail.com>
2025-06-09 21:13:06 +0300

    add an example spring boot app for an easier local demo

---
+++ src/test/java/dev/tnaumov/proxmox/ExampleApplication.java
@@ -0,0 +1,30 @@
+package dev.tnaumov.proxmox;
+
+import dev.tnaumov.proxmox.client.PveClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+@Profile("example")
+@SpringBootApplication
+public class ExampleApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ExampleApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(PveClient pveClient) {
+        return args -> {
+            log.info("Root API: {}", pveClient.get("/", String.class));
+        };
+
+    }
+
+}
